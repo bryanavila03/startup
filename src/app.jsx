31f98f3ed90991f6e95342c6e4279e3ee0304e4f
bryanavila03 <rufito28@gmail.com>
2026-02-11 16:39:37 -0700

    created the routing and the NavLink

--- src/app.jsx
+++ src/app.jsx
@@ -13,28 +13,45 @@ import { CustomerPortal } from './CustomerPortal/CustomerPortal';
 
 
 export default function App() {
-  return <div className="app bg-dark text-light">
+  return (
+  <BrowserRouter>
+  <div className="app bg-dark text-light">
     <header>
       <h1>Schedule an Appointment Today! (###) ###-####</h1>
       <nav className = "site-navbar">
          <ul>
-          <li id="home-link"><a href="Login.html">Phoenix Pest Control</a></li>
-          <li><a href="Services.html">Services</a></li>
-          <li><a href="AboutUs.html">About Us</a></li>
-          <li><a href="ContactUs.html">Contact Us</a></li>
-          <li><a href="SignIn.html">Sign In</a></li>      
+          <li id="home-link"><NavLink href="/">Phoenix Pest Control</NavLink></li>
+          <li><NavLink href="/Services">Services</NavLink></li>
+          <li><NavLink href="/AboutUs">About Us</NavLink></li>
+          <li><NavLink href="/ContactUs">Contact Us</NavLink></li>
+          <li><NavLink href="/SignIn">Sign In</NavLink></li>      
         </ul>
       </nav>
 
     </header>
 
-    <main className="container"> PlaceHolder </main>
+<Routes>
+  <Route path='/' element={<Login />} exact />
+  <Route path='/Services' element={<Services />} />
+  <Route path='/Scores' element={<Scores />} />
+  <Route path='/AboutUs' element={<AboutUs />} />
+  <Route path='/ContactUs' element={<ContactUs />} />
+  <Route path='/SignIn' element={<SignIn />} />
+  <Route path='/CustomerPortal' element={<CustomerPortal />} />
+  <Route path='*' element={<NotFound />} />
 
+</Routes>
 
     <footer>
       <hr />
       <span className="text-reset">Bryan Avila</span>
       <br />
        <a href="https://github.com/bryanavila03/startup.git">GitHub</a>
-    </footer></div>;
+    </footer>
+    </div>;
+  </BrowserRouter>
+  );
+}
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
