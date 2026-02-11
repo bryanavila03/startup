69f4aec339aab850c3c0320f2d809869c878ee2d
bryanavila03 <rufito28@gmail.com>
2026-02-11 15:42:11 -0700

    added header/footer/placeholder main to the app.jsx file

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="app bg-dark text-light">TEST!</div>;
+  return <div className="app bg-dark text-light">
+    <header>
+      <h1>Schedule an Appointment Today! (###) ###-####</h1>
+      <nav className = "site-navbar">
+         <ul>
+          <li id="home-link"><a href="Login.html">Phoenix Pest Control</a></li>
+          <li><a href="Services.html">Services</a></li>
+          <li><a href="AboutUs.html">About Us</a></li>
+          <li><a href="ContactUs.html">Contact Us</a></li>
+          <li><a href="SignIn.html">Sign In</a></li>      
+        </ul>
+      </nav>
+
+    </header>
+
+    <main className="container"> PlaceHolder </main>
+
+
+    <footer>
+      <hr />
+      <span className="text-reset">Bryan Avila</span>
+      <br />
+       <a href="https://github.com/bryanavila03/startup.git">GitHub</a>
+    </footer></div>;
 }
