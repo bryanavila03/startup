58dd3bac2802786c7522595e88787e533f156309
bryanavila03 <rufito28@gmail.com>
2026-02-26 14:52:46 -0700

    made it so when you are logged in and click the sign in page you aren't "logged out" and sent to sign in page. I tried using windows.location.href, but kept refreshing which was really annoying so used same redirection as customer portal

--- src/app.jsx
+++ src/app.jsx
@@ -44,7 +44,14 @@ export default function App() {
   <Route path='/Services' element={<Services />} />
   <Route path='/AboutUs' element={<AboutUs />} />
   <Route path='/ContactUs' element={<ContactUs />} />
-  <Route path='/SignIn' element={<SignIn setUser={setUser} />} />
+  <Route path='/SignIn' element={
+    user
+      ? <CustomerPortal user={user} setUser={setUser} />
+      : <SignIn setUser={setUser} /> 
+  } />
+   
+
+
   <Route path='/CustomerPortal' element={
     user
       ? <CustomerPortal user={user} setUser={setUser} />
