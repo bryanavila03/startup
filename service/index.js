165a5667d29dd8297b3d3542c472f9524543393c
Bryan Avila <rufito28@gmail.com>
2026-03-10 16:06:37 -0600

    created the port configuration so it goes to 3000 unless something is provided. Then created the server.

--- service/index.js
+++ service/index.js
@@ -2,3 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const uuidv4 = require('uuid');
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+const app = express();
