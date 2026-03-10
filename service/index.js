e3dfaab49364f96159fc74c4fc8287a42fb26f40
Bryan Avila <rufito28@gmail.com>
2026-03-10 16:44:22 -0600

    evreything will get rerouted to api so erased the /api, minor fixes, and the simon example used var instead of const for the apiRouter but since its never gonna change its not necessary.

--- service/index.js
+++ service/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
-const uuidv4 = require('uuid');
+const uuid = require('uuid');
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -11,10 +11,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
-app.get('/api/ping', (req, res) => {
+app.get('/ping', (req, res) => {
   res.json({ message: 'Server working!' });
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+let users = [];
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
