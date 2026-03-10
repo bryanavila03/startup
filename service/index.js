250433f74a72a288aacea323c695c45a1f807f84
Bryan Avila <rufito28@gmail.com>
2026-03-10 16:10:26 -0600

    created these lines to read JSON,cookies, and to get files from public folder

--- service/index.js
+++ service/index.js
@@ -6,3 +6,7 @@ const uuidv4 = require('uuid');
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
