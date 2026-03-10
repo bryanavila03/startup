ab42a0431407f14929b37c6906b1af6059f409d5
Bryan Avila <rufito28@gmail.com>
2026-03-10 16:27:07 -0600

    started server and tested its working

--- service/index.js
+++ service/index.js
@@ -3,10 +3,18 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const uuidv4 = require('uuid');
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'Server working!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
